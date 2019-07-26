d4f119f396c4b98e08922e3f51fad02e2eb11917
ryo <ba068082@gmail.com>
2019-07-27 07:19:21 +0900

    [from now] 2019/07/27 07:19:21
    diff --git a/.env b/.env
    index 95c5c7e..0ad3d29 100644
    --- a/.env
    +++ b/.env
    @@ -1 +1 @@
    -QIITA_TOKEN=2c0b6b9ac6576f8314fdaa2c9fd0abeef9195df9
    +VUE_APP_QIITA_TOKEN=2c0b6b9ac6576f8314fdaa2c9fd0abeef9195df9
    diff --git a/src/App.vue b/src/App.vue
    index aa3a3ff..6cb82aa 100644
    --- a/src/App.vue
    +++ b/src/App.vue
    @@ -1,10 +1,17 @@
     <template>
       <div id="app">
    -    <div id="nav">
    -      <router-link to="/">Home</router-link> |
    -      <router-link to="/about">About</router-link>
    -    </div>
    -    <router-view />
    +    <v-app>
    +      <div id="nav">
    +        <v-content>
    +          <v-container>
    +            <router-link to="/">Home</router-link> |
    +            <router-link to="/about">About</router-link>
    +          </v-container>
    +        </v-content>
    +      </div>
    +      <v-container>
    +        <router-view />
    +      </v-container>
    +    </v-app>
       </div>
     </template>
    -
    diff --git a/src/lib/api.ts b/src/lib/api.ts
    index 5107539..eaaa691 100644
    --- a/src/lib/api.ts
    +++ b/src/lib/api.ts
    @@ -1,11 +1,12 @@
     import axios from "axios";
    -const TOKEN = process.env.QIITA_TOKEN;
    
    -axios.defaults.baseURL = "https://qiita.com/api/v2/docs";
    +const TOKEN = process.env.VUE_APP_QIITA_TOKEN;
    +
    +axios.defaults.baseURL = "https://qiita.com/api/v2";
     axios.defaults.headers.common["Authorization"] = `Bearer ${TOKEN}`;
    
     export default {
       getMyAuthPosts: async () => {
    -    return await axios.get<Item[]>("/authenticated_user/items");
    +    return await axios.get<Post[]>("/authenticated_user/items");
       }
     };
    diff --git a/src/main.ts b/src/main.ts
    index 4fa2db3..33881dd 100644
    --- a/src/main.ts
    +++ b/src/main.ts
    @@ -3,11 +3,8 @@ import App from "./App.vue";
     import "./registerServiceWorker";
     import router from "./router";
     import { plugin } from "vue-function-api";
    -import dotenv from "dotenv";
     import vuetify from "./plugins/vuetify";
    
    -dotenv.config();
    -
     Vue.config.productionTip = false;
    
     Vue.use(plugin);
    diff --git a/src/views/Home.vue b/src/views/Home.vue
    index 7c28293..434cb6a 100644
    --- a/src/views/Home.vue
    +++ b/src/views/Home.vue
    @@ -1,5 +1,9 @@
     <template>
    -  <div class="home"></div>
    +  <div class="home">
    +    <v-container>
    +      <h1>hello</h1>
    +    </v-container>
    +  </div>
     </template>
    
     <script lang="ts">
    @@ -9,7 +13,7 @@ import {
       value,
       Wrapper
     } from "vue-function-api";
    -import api from "@/lib/api.js";
    +import api from "@/lib/api.ts";
    
     export default createComponent({
       setup() {

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <v-app>
+      <div id="nav">
+        <v-content>
+          <v-container>
+            <router-link to="/">Home</router-link> |
+            <router-link to="/about">About</router-link>
+          </v-container>
+        </v-content>
+      </div>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-app>
   </div>
 </template>
-
